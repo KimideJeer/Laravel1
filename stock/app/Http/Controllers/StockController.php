01fa261e8ac8ce31e26626aaf2cadbbe0d20bc0b
Kimi de Jeer <Kimi.deJeer@student.rocva.nl>
2024-11-20 11:34:16 +0100

    Create

--- stock/app/Http/Controllers/StockController.php
+++ stock/app/Http/Controllers/StockController.php
@@ -20,17 +20,30 @@ class StockController extends Controller
      * Show the form for creating a new resource.
      */
     public function create()
-    {
-        //
-    }
+{
+    return view('stocks.create'); // -> resources/views/stocks/create.blade.php
+}
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        //
-    }
+public function store(Request $request)
+{
+    // Validation for required fields (and using some regex to validate our numeric value)
+    $request->validate([
+        'stock_name'=>'required',
+        'ticket'=>'required',
+        'value'=>'required|max:10|regex:/^-?[0-9]+(?:\.[0-9]{1,2})?$/'
+    ]); 
+    // Getting values from the blade template form
+    $stock = new Stock([
+        'stock_name' => $request->get('stock_name'),
+        'ticket' => $request->get('ticket'),
+        'value' => $request->get('value')
+    ]);
+    $stock->save();
+    return redirect('/stocks')->with('success', 'Stock saved.');   // -> resources/views/stocks/index.blade.php
+}
 
     /**
      * Display the specified resource.
--- stock/routes/web.php
+++ stock/routes/web.php
@@ -6,3 +6,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('stocks', [App\Http\Controllers\StockController::class, 'index']);
+
+Route::get('stocks/create', [App\Http\Controllers\StockController::class, 'create']);
+
+Route::get('stocks/store', [App\Http\Controllers\StockController::class, 'store']);
